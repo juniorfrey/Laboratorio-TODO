c6e09a6beba0863e6f9a5b70ecb1c4ac6aef4209
Fredys Fernandez <juniorfrey2990@gmail.com>
2021-11-10 10:35:34 -0500

    Se implemento el metodo de eliminar una tarea. funcionando OK

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -15,6 +15,7 @@ export class TodoList{
 
     eliminarTodo ( id ){
 
+        this.todos =   this.todos.filter( todo => todo.id != id );
     }
 
     marcarCompletado(id){
--- src/components/componentes.js
+++ src/components/componentes.js
@@ -49,7 +49,8 @@ divTodoList.addEventListener('click', (event) => {
     if( nombreElemento.includes('input') ){
         todoList.marcarCompletado(todoId);
         todoElemento.classList.toggle('completed')
+    }else if(nombreElemento.includes('button') ){
+        todoList.eliminarTodo( todoId );
+        divTodoList.removeChild( todoElemento );
     }
-
-    console.log(todoList);
-})
+});
